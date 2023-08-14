0586e96e11892974a791e56447bbeaada99c6ae1
redlik <ralphr@outlook.com>
2023-08-14 20:23:52 +0100

    Tailwind initialised

---
+++ web/themes/custom/tailwind/tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./templates/**/*.twig"],
+  theme: {
+    extend: {},
+  },
+  plugins: ["tailwindcss", "postcss-nesting", "autoprefixer"],
+}
