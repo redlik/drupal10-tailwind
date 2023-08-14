ccc7044c448f1a19ee4c172bd0ca39c2e558a8e7
redlik <ralphr@outlook.com>
2023-08-14 21:51:27 +0100

    Inter added to tailwind

--- web/themes/custom/tailwind/tailwind.config.js
+++ web/themes/custom/tailwind/tailwind.config.js
@@ -2,6 +2,9 @@
 module.exports = {
   content: ["./templates/**/*.twig"],
   theme: {
+    fontFamily: {
+      'sans': ['Inter', 'sans-serif'],
+    },
     extend: {},
   },
   plugins: ["tailwindcss", "postcss-nesting", "autoprefixer"],
